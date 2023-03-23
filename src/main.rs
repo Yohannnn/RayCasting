9478a67daf8ff7dcd1ed5d517c5b97b366b193b5
Yohan <johanfriesen16@icloud.com>
2023-03-23 14:54:25 -0700

    Added basic floor and ceiling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::rect:: Rect;
-use sdl2::render::Texture;
-use std::time::{Duration, Instant};
 use sdl2::image::LoadTexture;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Texture;
 use std::ops;
-
+use std::time::{Duration, Instant};
 
 // Screen size
 const SCREEN_WIDTH: u32 = 640;
@@ -28,9 +27,12 @@ impl ops::Neg for V<f64> {
     type Output = V<f64>;
 
     fn neg(self) -> V<f64> {
-        V{x: -self.x, y: -self.y}
+        V {
+            x: -self.x,
+            y: -self.y,
+        }
     }
-} 
+}
 
 // State struct
 #[derive(Debug, Copy, Clone)]
@@ -40,6 +42,7 @@ struct State {
     plane: V<f64>,
 }
 
+//TODO: Load map from file
 // Map of the walls
 const MAP: [[u8; 24]; 24] =[
   [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,7,7,7,7,7,7,7,7],
@@ -68,7 +71,6 @@ const MAP: [[u8; 24]; 24] =[
   [4,4,4,4,4,4,4,4,4,4,1,1,1,2,2,2,2,2,2,3,3,3,3,3]
 ];
 
-
 // Rotate the player and the camera plane
 fn rotate(state: &mut State, angle: f64) {
     let (s, c) = angle.sin_cos();
@@ -82,7 +84,7 @@ fn rotate(state: &mut State, angle: f64) {
 
 // Move the player
 fn move_player(s: &mut State, forward: bool) {
-    let dir = if forward {s.dir} else {-s.dir};
+    let dir = if forward { s.dir } else { -s.dir };
     let move_x = s.pos.x + dir.x * MOV_SPEED;
     let move_y = s.pos.y + dir.y * MOV_SPEED;
 
@@ -99,20 +101,22 @@ fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
-    let window = video_subsystem.window("raycaster", SCREEN_WIDTH, SCREEN_HEIGHT)
+    let window = video_subsystem
+        .window("raycaster", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
-    let mut canvas = window.into_canvas()
+    let mut canvas = window
+        .into_canvas()
         .accelerated()
         .present_vsync()
         .build()
         .expect("could not make a canvas");
 
     let mut event_pump = sdl_context.event_pump()?;
-    
-    // TODO: Switch to textures loaded from files
+
+    // Load in the textures from files
     let texture_creator = canvas.texture_creator();
     let textures: [Texture; 8] = [
         texture_creator.load_texture("assets/eagle.png")?,
@@ -127,10 +131,10 @@ fn main() -> Result<(), String> {
 
     let mut state = State {
         pos: V { x: 4.5, y: 5.0 },
-        dir: V { x:-1.0, y: 0.0 },
+        dir: V { x: -1.0, y: 0.0 },
         plane: V { x: 0.0, y: 0.66 },
-     };
-    
+    };
+
     let mut movement_keys = [false; 4];
 
     'running: loop {
@@ -141,37 +145,72 @@ fn main() -> Result<(), String> {
         for event in event_pump.poll_iter() {
             match event {
                 // Detect window close or escape key
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
-                },
+                }
 
                 // Movement input
-                Event::KeyDown { keycode: Some(Keycode::W), repeat: false, .. } => {
+                Event::KeyDown {
+                    keycode: Some(Keycode::W),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[0] = true;
-                },
-                Event::KeyDown { keycode: Some(Keycode::S), repeat: false, .. } => {
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::S),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[1] = true;
-                },
-                Event::KeyDown { keycode: Some(Keycode::A), repeat: false, .. } => {
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[2] = true;
-                },
-                Event::KeyDown { keycode: Some(Keycode::D), repeat: false, .. } => {
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::D),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[3] = true;
-                },
+                }
 
-                Event::KeyUp { keycode: Some(Keycode::W), repeat: false, .. } => {
+                Event::KeyUp {
+                    keycode: Some(Keycode::W),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[0] = false;
-                },
-                Event::KeyUp { keycode: Some(Keycode::S), repeat: false, .. } => {
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::S),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[1] = false;
-                },
-                Event::KeyUp { keycode: Some(Keycode::A), repeat: false, .. } => {
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::A),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[2] = false;
-                },
-                Event::KeyUp { keycode: Some(Keycode::D), repeat: false, .. } => {
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::D),
+                    repeat: false,
+                    ..
+                } => {
                     movement_keys[3] = false;
-                },
+                }
                 _ => {}
             }
         }
@@ -188,15 +227,19 @@ fn main() -> Result<(), String> {
         if movement_keys[2] {
             rotate(&mut state, ROT_SPEED)
         }
-        
+
         if movement_keys[3] {
             rotate(&mut state, -ROT_SPEED)
         }
 
         // Render
-        // Clear the screen to black
-        canvas.set_draw_color(Color::RGB(0, 0, 0));
-        canvas.clear();
+        // TODO: Add a texture for the floor and ceiling
+        // Draw floor and ceiling
+        canvas.set_draw_color(Color::RGB(0, 0, 255));
+        canvas.fill_rect(Rect::new(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT / 2))?;
+        canvas.set_draw_color(Color::RGB(0, 255, 0));
+        canvas.fill_rect(Rect::new(  0, (SCREEN_HEIGHT / 2) as i32, SCREEN_WIDTH, SCREEN_HEIGHT / 2,))?;
+
 
         // Render the walls
         for x in 0..SCREEN_WIDTH {
@@ -204,19 +247,28 @@ fn main() -> Result<(), String> {
             let camera_x = 2.0 * x as f64 / SCREEN_WIDTH as f64 - 1.0;
 
             // Ray direction
-            let ray = V{x:state.dir.x + state.plane.x * camera_x, y: state.dir.y + state.plane.y * camera_x};
+            let ray = V {
+                x: state.dir.x + state.plane.x * camera_x,
+                y: state.dir.y + state.plane.y * camera_x,
+            };
 
             // Position of the player on the map as an int
-            let mut map_pos = V{x:state.pos.x as i32, y: state.pos.y as i32};
+            let mut map_pos = V {
+                x: state.pos.x as i32,
+                y: state.pos.y as i32,
+            };
 
             // Length of the ray from one x or y-side to the next x or y-side
-            let delta_dist = V{x: (1.0 / ray.x).abs(), y: (1.0 / ray.y).abs()};
+            let delta_dist = V {
+                x: (1.0 / ray.x).abs(),
+                y: (1.0 / ray.y).abs(),
+            };
 
             // Value to increment the map pos by
-            let mut step = V{x: 0, y: 0};
+            let mut step = V { x: 0, y: 0 };
 
             // Distance to the next x or y-side
-            let mut side_dist = V{x: 0.0, y: 0.0};
+            let mut side_dist = V { x: 0.0, y: 0.0 };
 
             // Length of the ray from the wall hit to the camera plane
             let perp_wal_dist: f64;
@@ -230,7 +282,7 @@ fn main() -> Result<(), String> {
                 side_dist.x = (state.pos.x - map_pos.x as f64) * delta_dist.x;
             } else {
                 step.x = 1;
-                side_dist.x = (map_pos.x as f64 + 1.0 -state.pos.x) * delta_dist.x;
+                side_dist.x = (map_pos.x as f64 + 1.0 - state.pos.x) * delta_dist.x;
             }
 
             if ray.y < 0.0 {
@@ -238,7 +290,7 @@ fn main() -> Result<(), String> {
                 side_dist.y = (state.pos.y - map_pos.y as f64) * delta_dist.y;
             } else {
                 step.y = 1;
-                side_dist.y = (map_pos.y as f64 + 1.0 -state.pos.y) * delta_dist.y;
+                side_dist.y = (map_pos.y as f64 + 1.0 - state.pos.y) * delta_dist.y;
             }
 
             // Perform DDA
@@ -255,7 +307,9 @@ fn main() -> Result<(), String> {
                 }
 
                 // Check if the ray has hit a wall
-                if MAP[map_pos.y as usize][map_pos.x as usize] > 0 {break;}
+                if MAP[map_pos.y as usize][map_pos.x as usize] > 0 {
+                    break;
+                }
             }
 
             // Calculate the distance projected on the camera direction
@@ -278,18 +332,18 @@ fn main() -> Result<(), String> {
             // Calculate the value of the wall x coordinate
             let mut wall_x: f64;
             if ns_side {
-                wall_x =state.pos.x + perp_wal_dist * ray.x;
+                wall_x = state.pos.x + perp_wal_dist * ray.x;
             } else {
-                wall_x =state.pos.y + perp_wal_dist * ray.y;
+                wall_x = state.pos.y + perp_wal_dist * ray.y;
             }
             wall_x -= wall_x.floor();
 
             // Get the x coordinate on the texture
             let mut tex_x = (wall_x * TEX_SIZE as f64) as u32;
-            if ns_side && ray.y < 0.0{
+            if ns_side && ray.y < 0.0 {
                 tex_x = TEX_SIZE - tex_x - 1;
             }
-            if !ns_side && ray.x > 0.0{
+            if !ns_side && ray.x > 0.0 {
                 tex_x = TEX_SIZE - tex_x - 1;
             }
 
